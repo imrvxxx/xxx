bab928cbe4be84e6cc75cd910f734f2b3524a929
imrvxxx <baldo.sgll06@gmail.com>
2026-07-26 17:31:53 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: " ", // required by Discord API, not shown in the client
         state: "hi uno",     // this is what people actually see
-        type: 1,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
